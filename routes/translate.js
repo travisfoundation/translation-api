e93d340ea09694de90e02c8c2221e82e97a20dae
R van der Feer <r.vanderfeer@primadeluxe.com>
2019-03-13 17:22:09 +0100

    adding implementation for Q42's translation enpoint

--- routes/translate.js
+++ routes/translate.js
@@ -1,46 +1,47 @@
 var restify = require('restify');
+var request = require('request');
 
 module.exports.routes = function(server) {
 
 	server.post('/translate', function respond(req, res, next) {
-
-		console.log('/translate'.green, req.params);
-
-		res.send({
-			source: req.params.source_lang,
-			target: req.params.target_lang,
-			translations: [
-				{
-					text: "ንሕና ንሰባት ክንርድኦም ደሊና ።"
+		request.post(
+			'https://hacking-tigrinya.appspot.com/translate/'+req.params.source_lang+'-'+req.params.target_lang,
+			{
+				json: {
+					"instances": [req.params.phrase]
 				}
-			]
-		});
+			},
+			function (error, response, body) {
+				if (!error && response.statusCode == 200) {
+					console.log('/translate'.green, req.params, body);
+
+					var text = body.predictions.join(' ').replace('. ,','.').trim().replace(' .', '.');
+
+					res.send({
+						source: req.params.source_lang,
+						target: req.params.target_lang,
+						translations: [
+							{
+								text: text
+							}
+						]
+					});
+	        	} else {
+					console.error('/translate'.red, req.params, response);
+	        	}
+			}
+		);
+
+
+		// 	res.send({
+		// 	source: req.params.source_lang,
+		// 	target: req.params.target_lang,
+		// 	translations: [
+		// 		{
+		// 			text: "ንሕና ንሰባት ክንርድኦም ደሊና ።"
+		// 		}
+		// 	]
+		// });
 		return next();
 	});
-
-	// server.post('/translate', rateLimit, function(req, res, next) {
-	// 	// Delete for these dates first
-	// 	req.db.BusyDay.destroy( { where: { date: req.params.dates } } ).then(function(data)
-	// 	{
-	// 		// If the level is not 1, re-insert it (level 1 has no status)
-	// 		if(req.params.level > 1) {
-	// 			var entries = [];
-	// 			for(var i = 0; i < req.params.dates.length; ++i)
-	// 			{
-	// 				entries.push( { date: req.params.dates[i], busy_level: req.params.level } );
-	// 			}
-
-	// 			req.db.BusyDay.bulkCreate(entries).then(function(data)
-	// 			{
-	// 				res.send({});
-	// 				return next();
-	// 			});
-	// 		}
-	// 		else {
-	// 			res.send({});
-	// 			return next();
-	// 		}
-	// 	});
-	// });
-
 };
